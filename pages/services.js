76ffb23540563a22b8fa1d5b9e4961144f50cb89
DomeNTPS <tanndome_555@hotmail.com>
2020-09-30 16:17:59 +0700

    service page done

--- pages/services.js
+++ pages/services.js
@@ -49,30 +49,15 @@ function services() {
         <div className="customer-service">Customer Service</div>
         <div className="service-icon">
           <div className="icon">
-            <img
-              src="/dashboard.svg"
-              alt=""
-              height={230}
-              style={{ marginLeft: 90, marginTop: 40 }}
-            />
+            <img src="/dashboard.svg" alt="" className="img-responsive" />
             <div className="icon-text">Dashboard for Monitoring</div>
           </div>
           <div className="icon">
-            <img
-              src="/result.svg"
-              alt=""
-              height={230}
-              style={{ marginLeft: 90, marginTop: 40 }}
-            />
+            <img src="/result.svg" alt="" className="img-responsive" />
             <div className="icon-text">Export results with Excel/PDF file</div>
           </div>
           <div className="icon">
-            <img
-              src="/callservice.svg"
-              alt=""
-              height={230}
-              style={{ marginLeft: 90, marginTop: 40 }}
-            />
+            <img src="/callservice.svg" alt="" className="img-responsive" />
             <div className="icon-text">24 hour support</div>
           </div>
         </div>
